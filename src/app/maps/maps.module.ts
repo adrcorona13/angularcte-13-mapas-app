494048a01b2a300577d1a4ec8e69bf9d97482213
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-10 19:11:53 -0600

    crear map screen component

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MapScreenComponent } from './screens/map-screen/map-screen.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MapScreenComponent
+  ],
   imports: [
     CommonModule
   ]
