34e16525d5004823f4a3d1ecc5969e16f412bf7f
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-10 19:27:48 -0600

    crear place service para obtener geolocation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if (!navigator.geolocation) {
+  const msg = 'Geolocation is not available';
+  alert(msg);
+  throw new Error(msg);
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
