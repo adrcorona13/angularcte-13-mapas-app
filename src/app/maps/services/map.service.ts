7e1d8791ff5e3b973cfd0fcf064f4a5f764f0f29
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-12 13:13:59 -0600

    ajustar mapa para visualizar los resultados encontrados

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import { LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
 import { Feature } from '../interfaces/places.interface';
 
 @Injectable({
@@ -30,8 +30,9 @@ export class MapService {
     })
   }
 
-  createMarkersFromPlaces(places: Feature[]){
+  createMarkersFromPlaces(places: Feature[], userLocation: [number, number]){
     if (!this.map) throw Error('El mapa no esta inicializado');
+    
     this.markers.forEach(marker => marker.remove());
     const newMarkers = [];
     for(const place of places){
@@ -49,6 +50,18 @@ export class MapService {
     }
 
     this.markers = newMarkers;
+
+    if (places.length === 0) return;
+
+    const bounds = new LngLatBounds();
+
+    newMarkers.forEach(marker => bounds.extend(marker.getLngLat()));
+
+    bounds.extend(userLocation);
+
+    this.map.fitBounds(bounds, {
+      padding: 200
+    });
   }
   
 
