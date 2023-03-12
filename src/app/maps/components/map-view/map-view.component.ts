450e9f129453ba48e4128d8edf7688626eddae9c
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-11 20:55:16 -0600

    agregar mapbox

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { PlacesService } from '../../services';
+import {Map} from 'mapbox-gl';
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.css']
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent implements AfterViewInit {
+
+  @ViewChild('map') 
+  mapElement!: ElementRef
 
   constructor(private placesService: PlacesService) { }
 
-  ngOnInit(): void {
-    console.log(this.placesService.useLocation);
-    
+  ngAfterViewInit(): void {
+    if (!this.placesService.useLocation) {
+      throw Error('Cannot get user location');
+    }
+    const map = new Map({
+      container: this.mapElement.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.placesService.useLocation, // starting position [lng, lat]
+      zoom: 17, // starting zoom
+      });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWRyY29yb25hMTMiLCJhIjoiY2xlbDFodXFnMDZ2aDN3bWp3aDQwMDgzZCJ9.acvpRtp4CJNVIabIUyC9PA';
 
 if (!navigator.geolocation) {
   const msg = 'Geolocation is not available';
